e2e6be4f5dafebabd4e4280a55e1db3fa85403f6
Yuriy Stos <100sbsh@gmail.com>
2016-10-27 14:00:10 +0300

    Deleted unnecessary TODO

--- index.php
+++ index.php
@@ -2,9 +2,3 @@
 require_once 'classes/ConfigHelper.php';
 require_once 'classes/DbConnection.php';
 require_once 'classes/Vocabulary.php';
-
-$vocabulary = new Vocabulary();
-//TODO: delete this
-echo "<pre>";
-print_r(var_export($vocabulary->getVocabulary(), true));
-echo "</pre>";
