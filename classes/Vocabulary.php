b211333471b6aa8b8bc75def79a9389ed500bca3
Yuriy Stos <100sbsh@gmail.com>
2016-11-02 17:05:10 +0200

    Added lookup select from vocabulary

--- classes/Vocabulary.php
+++ classes/Vocabulary.php
@@ -7,10 +7,20 @@
  */
 class Vocabulary
 {
+    /**
+     * Get vocabulary objects
+     *
+     * @return array - Array with std objects with 'stem' and 'lookup' fields
+     */
     public function getVocabulary()
     {
         $connection = DbConnection::getInstance()->getConnection();
-        $query = "SELECT `w`.`stem` FROM `WORDS` `w`";
+
+        $query = <<< SQL
+SELECT `w`.`stem`, `l`."usage" FROM `WORDS` `w`
+  JOIN LOOKUPS `l`
+    ON `w`.id=`l`.word_key
+SQL;
         $smt = $connection->prepare($query);
         $smt->execute();
         return $smt->fetchAll(PDO::FETCH_OBJ);
