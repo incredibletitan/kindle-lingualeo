7edf7896e433e9a46541ef717fe7068ae102a679
Yuriy Stos <100sbsh@gmail.com>
2016-11-04 13:52:53 +0200

    Added linguleo credentials configuration

--- config/config.php
+++ config/config.php
@@ -1,4 +1,7 @@
 <?php
 return [
-    'path_to_vocabulary' => ''
+    'path_to_vocabulary' => '',
+    'lingualeo_api_url' => 'http://api.lingualeo.com',
+    'lingualeo_user_email' => '',
+    'lingualeo_user_password' => '',
 ];
