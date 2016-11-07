84b1a9cbc5825655cd6969f6ece956938f2a0dd6
Yuriy Stos <100sbsh@gmail.com>
2016-11-07 13:41:22 +0200

    Added test class for LinguaLeo

---
+++ tests/components/LinguaLeoTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace tests\components;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class LinguaLeoTest
+ */
+class LinguaLeoTest extends TestCase
+{
+    /**
+     * @covers libs\components\LinguaLeo::authorize
+     */
+    public function testAuthorize()
+    {
+
+    }
+}
