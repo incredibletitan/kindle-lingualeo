707aab4957479f706ac396e01f844966de1ed435
Yuriy Stos <100sbsh@gmail.com>
2016-12-01 13:45:54 +0200

    Started to write testGetResponse method

--- tests/components/LinguaLeoTest.php
+++ tests/components/LinguaLeoTest.php
@@ -27,12 +27,49 @@ class LinguaLeoTest extends TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $dummyUrl = 'dummy_uri';
+        $dummyUrl = 'dummy_url';
 
         $stub->method('getContentByUrl')
             ->willReturn('dummy_content')
             ->willThrowException(new RequestException('Dummy request exception', new Request('GET', $dummyUrl)));
 
         $method->invoke($stub, $dummyUrl);
+    }
+
+    /**
+     * @covers LinguaLeo::getResponse
+     */
+    public function testGetResponse()
+    {
+        $reflection = new \ReflectionClass('libs\components\LinguaLeo');
+        $method = $reflection->getMethod('getResponse');
+        $method->setAccessible(true);
+
+        $stub = $this->getMockBuilder('libs\components\LinguaLeo')
+            ->setMethods(['getContentByUrl'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $dummyUrl = 'dummy_uri';
+
+        // Create a map of arguments to return values.
+        $map = [
+            'dummy_content',
+            '[{"test1": "1","test2": "2"}]'
+        ];
+
+
+        //Without JSON encode
+        $stub->expects($this->any())
+            ->method('getContentByUrl')
+            ->will($this->returnValueMap($map));
+
+//        $this->assertEquals('dummy_content', $method->invoke($stub, $dummyUrl, [], false));
+//
+//        //With JSON encode
+//        $stub->method('getContentByUrl')
+//            ->willReturn('[{"test1": "1","test2": "2"}]');
+
+        $this->assertEquals('dummy_content', $method->invoke($stub, $dummyUrl));
     }
 }
