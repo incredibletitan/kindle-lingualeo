6c1b204f5efce37cf25abed403556f98bce698ec
Yuriy Stos <100sbsh@gmail.com>
2016-12-02 14:04:52 +0200

    Improvements in LinguaLeoTest class

--- tests/components/LinguaLeoTest.php
+++ tests/components/LinguaLeoTest.php
@@ -4,6 +4,7 @@ namespace tests\components;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\Request;
 use PHPUnit\Framework\TestCase;
+use libs\components\LinguaLeoApiException;
 
 /**
  * Class LinguaLeoTest
@@ -14,18 +15,12 @@ class LinguaLeoTest extends TestCase
      * Tests exception rethrowing in case of Guzzle throw RequestException
      *
      * @covers LinguaLeo::getResponse
-     * @expectedException libs\components\LinguaLeoApiException
+     * @expectedException \libs\components\LinguaLeoApiException
      */
     public function testLinguaLeoApiExceptionThrowsInGetResponse()
     {
-        $reflection = new \ReflectionClass('libs\components\LinguaLeo');
-        $method = $reflection->getMethod('getResponse');
-        $method->setAccessible(true);
-
-        $stub = $this->getMockBuilder('libs\components\LinguaLeo')
-            ->setMethods(['getContentByUrl'])
-            ->disableOriginalConstructor()
-            ->getMock();
+        $method = $this->getReflectedMethod('getResponse');
+        $stub = $this->getMockedObject(['getContentByUrl']);
 
         $dummyUrl = 'dummy_url';
 
@@ -41,35 +36,62 @@ class LinguaLeoTest extends TestCase
      */
     public function testGetResponse()
     {
-        $reflection = new \ReflectionClass('libs\components\LinguaLeo');
-        $method = $reflection->getMethod('getResponse');
-        $method->setAccessible(true);
+        $method = $this->getReflectedMethod('getResponse');
+        $stub = $this->getMockedObject(['getContentByUrl']);
 
-        $stub = $this->getMockBuilder('libs\components\LinguaLeo')
-            ->setMethods(['getContentByUrl'])
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $dummyUrl = 'dummy_uri';
-
-        // Create a map of arguments to return values.
-        $map = [
-            'dummy_content',
-            '[{"test1": "1","test2": "2"}]'
-        ];
-
+        $dummyUrl = 'dummy_url';
 
         //Without JSON encode
-        $stub->expects($this->any())
+        $stub->expects($this->at(0))
             ->method('getContentByUrl')
-            ->will($this->returnValueMap($map));
+            ->willReturn('dummy_content');
 
-//        $this->assertEquals('dummy_content', $method->invoke($stub, $dummyUrl, [], false));
-//
-//        //With JSON encode
-//        $stub->method('getContentByUrl')
-//            ->willReturn('[{"test1": "1","test2": "2"}]');
+        $this->assertEquals('dummy_content', $method->invoke($stub, $dummyUrl, [], false));
 
-        $this->assertEquals('dummy_content', $method->invoke($stub, $dummyUrl));
+        //With JSON encode
+        $stub->expects($this->at(0))
+            ->method('getContentByUrl')
+            ->willReturn('[{"test1": "1","test2": "2"}]');
+
+        $this->assertEquals([["test1" => '1', "test2" => '2']], $method->invoke($stub, $dummyUrl));
+
+        //Try to convert not valid JSON must throw LinguaLeoApiException
+        $stub->expects($this->at(0))
+            ->method('getContentByUrl')
+            ->willReturn('[{test1": "1","test2": "2"}]');
+
+        $method->invoke($stub, $dummyUrl);
+
+        $this->expectException(LinguaLeoApiException::class);
+    }
+
+    /**
+     * Returns private or protected method got by ReflectionClass
+     *
+     * @param string $method
+     *
+     * @return \ReflectionMethod|null
+     */
+    private function getReflectedMethod($method)
+    {
+        $reflection = new \ReflectionClass('libs\components\LinguaLeo');
+        $method = $reflection->getMethod($method);
+        $method->setAccessible(true);
+
+        return $method;
+    }
+
+    /**
+     * Get mock of LinguaLeo class
+     *
+     * @param array $allowedMethods - Methods for mocking
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getMockedObject(array $allowedMethods)
+    {
+        return $this->getMockBuilder('libs\components\LinguaLeo')
+            ->setMethods($allowedMethods)
+            ->disableOriginalConstructor()
+            ->getMock();
     }
 }
